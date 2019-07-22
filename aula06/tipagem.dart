e1fa1f85ea121b60e6904f4a1b0668d7a75d6990
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2019-07-21 21:53:53 -0300

    Começando o módulo 5 do curso

---
+++ aula06/tipagem.dart
@@ -0,0 +1,11 @@
+main(List<String> args) {
+  int num2 = 1;
+  double num1 = 1.2;
+  String text = "Israel Souza";
+  bool isTrue = true;
+
+  print(num2);
+  print(num1);
+  print(text);
+  print(isTrue);
+}
