2138af83c4d2b290b0722d459df81f109cec17e5
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2019-07-21 21:29:06 -0300

    Refatorando o código

--- aula05/labs/calculo_imc.dart
+++ aula05/labs/calculo_imc.dart
@@ -1,26 +1,32 @@
 import 'dart:io';
 import 'dart:math';
 
-main(List<String> args){
+main(List<String> args) {
   print("Digite o seu peso: ");
-  var peso  = double.parse(stdin.readLineSync());
+  var peso = double.parse(stdin.readLineSync());
 
   print("Digite sua altura: ");
   var altura = double.parse(stdin.readLineSync());
 
-  double imc = peso / pow(altura, 2);
+  mostrarImc(peso, altura);
+}
 
-  print(imc);
+mostrarImc(double peso, double altura) {
+  double imc = calcImc(peso, altura);
 
-  if(imc < 18.5){
+  if (imc < 18.5) {
     print("Abaixo do Peso");
-  }else if(imc >= 18.5 && imc < 25){
+  } else if (imc >= 18.5 && imc < 25) {
     print("Peso Normal");
-  }else if(imc >= 25 && imc < 30 ){
+  } else if (imc >= 25 && imc < 30) {
     print("Sobrepeso");
-  }else if(imc >= 30 && imc < 35){
+  } else if (imc >= 30 && imc < 35) {
     print("Obesidade Grau 1");
-  }else{
+  } else {
     print("Obesidade Grau 2");
   }
+}
+
+double calcImc(double peso, double altura) {
+  return peso / pow(altura, 2);
 }
