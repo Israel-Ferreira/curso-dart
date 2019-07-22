aebc6db7e81002c2bc0d127ad93bf24d97326ac5
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2019-07-21 21:22:52 -0300

    Terminando o algoritmo do imc

--- aula05/labs/calculo_imc.dart
+++ aula05/labs/calculo_imc.dart
@@ -1,4 +1,26 @@
 import 'dart:io';
+import 'dart:math';
 
 main(List<String> args){
+  print("Digite o seu peso: ");
+  var peso  = double.parse(stdin.readLineSync());
+
+  print("Digite sua altura: ");
+  var altura = double.parse(stdin.readLineSync());
+
+  double imc = peso / pow(altura, 2);
+
+  print(imc);
+
+  if(imc < 18.5){
+    print("Abaixo do Peso");
+  }else if(imc >= 18.5 && imc < 25){
+    print("Peso Normal");
+  }else if(imc >= 25 && imc < 30 ){
+    print("Sobrepeso");
+  }else if(imc >= 30 && imc < 35){
+    print("Obesidade Grau 1");
+  }else{
+    print("Obesidade Grau 2");
+  }
 }
